57f6c487f80d893453e3f0daffc00b1e435c9a5b
RikBala <rik280491@googlemail.com>
2020-09-22 23:59:19 +0100

    routing from launch to feed

--- src/app/launch/LaunchPage.js
+++ src/app/launch/LaunchPage.js
@@ -1,13 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import "./LaunchPage.css";
 import logo from "../../images/paw-icon.png";
+import AllCats from "../feed/AllCats";
 
 const LaunchPage = () => {
+	const [load, setLoad] = useState(false);
+
+	const loadFeed = () => {
+		setLoad(true);
+		document.querySelector("html").style.backgroundColor = "white";
+	};
+
 	return (
-		<div>
-			<img id="catsat__logo" src={logo} alt="Catsat Logo" />
-			<h1 id="launch__title">Catsat</h1>
-		</div>
+		<>
+			{load ? (
+				<AllCats />
+			) : (
+				<div id="launch__container" onClick={loadFeed}>
+					<img id="catsat__logo" src={logo} alt="Catsat Logo" />
+					<h1 id="launch__title">Catsat</h1>
+				</div>
+			)}
+		</>
 	);
 };
 
--- src/app/layout/AppRouter.js
+++ src/app/layout/AppRouter.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { Switch, Route } from "react-router";
 import LaunchPage from "../launch/LaunchPage";
+import AllCats from "../feed/AllCats";
 
 const AppRouter = () => (
 	<Switch>
-		<Route component={LaunchPage} />
+		<Route path="/launch" component={LaunchPage} />
+		<Route path="/feed" component={AllCats} />
 	</Switch>
 );
 
