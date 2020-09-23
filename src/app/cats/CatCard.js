72cca9a0b7ce47b14110d38e9c2fab9af9b48698
RikBala <rik280491@googlemail.com>
2020-09-23 21:48:31 +0100

    cat feed and all cat cards rendered

--- src/app/cats/CatCard.js
+++ src/app/cats/CatCard.js
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
+import "./CatCard.css";
+import emptyHeart from "../../images/heart.png";
+import filledHeart from "../../images/filled-heart.png";
 
-const CatCard = () => {
-	return <div>TEST</div>;
+const CatCard = ({ name, owner, image }) => {
+	const [likedCat, setLikedCat] = useState(false);
+
+	const likeCat = () => {
+		if (!likedCat) {
+			setLikedCat(true);
+		} else {
+			setLikedCat(false);
+		}
+	};
+
+	return (
+		<div id="cat__card">
+			<img id="cat__card-image" src={image} alt={`${name}, the cat`} />
+			<h2 id="cat__card-name">{name}</h2>
+			<p id="cat__card-owner">{`Owned by ${owner}`}</p>
+			<div id="cat__card-like">
+				<img
+					onClick={likeCat}
+					src={likedCat ? filledHeart : emptyHeart}
+					alt="heart this cat"
+				/>
+			</div>
+		</div>
+	);
 };
 
 export default CatCard;
