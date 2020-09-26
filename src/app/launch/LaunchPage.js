b2ea4a00860ffc4e7f1442c18d727e8700705cce
RikBala <rik280491@googlemail.com>
2020-09-26 17:47:10 +0100

    cat feed route working

--- src/app/launch/LaunchPage.js
+++ src/app/launch/LaunchPage.js
@@ -1,22 +1,27 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./LaunchPage.css";
 import CatsatLogoSVG from "../../images/paw-icon.svg";
 import FeedContainer from "../feed/FeedContainer";
+import { useHistory } from "react-router-dom";
 
 const LaunchPage = () => {
+	const history = useHistory();
 	const [load, setLoad] = useState(false);
 
-	const loadFeed = () => {
-		setLoad(true);
-		document.querySelector("html").style.backgroundColor = "white";
-	};
+	useEffect(() => {
+		setTimeout(() => {
+			setLoad(true);
+			document.querySelector("html").style.backgroundColor = "white";
+			history.push("/feed");
+		}, 2000);
+	});
 
 	return (
 		<>
 			{load ? (
 				<FeedContainer />
 			) : (
-				<div id="launch__container" onClick={loadFeed}>
+				<div id="launch__container">
 					<img id="catsat__logo" src={CatsatLogoSVG} alt="Catsat Logo" />
 					<h1 id="launch__title">Catsat</h1>
 				</div>
